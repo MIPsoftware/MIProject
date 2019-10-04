e0768e11cc18bfdb3ddb749c079fd132ef56fd16
martin080 <45700603+martin080@users.noreply.github.com>
2019-10-04 20:17:02 +0300

    Update Login.cs

--- MIPChat/Models/Login.cs
+++ MIPChat/Models/Login.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace MIPChat.Models
 {
-    public class Login
+    public class LoginModel
     {
         public string Email { get; set; }
         public string Password { get; set; }
