696662a5cc8be0c4b2171cff18621ce6ed337feb
DaneArt <public.centaurus@gmail.com>
2019-10-05 21:08:54 +0300

    Модели готовы

---
+++ MIPChat/DAL/Domain/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.ComponentModel.DataAnnotations;
+
+using System.Linq;
+using System.Web;
+
+namespace MIPChat.Models
+{
+    public class Message
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public Guid AuthorId { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public DateTime TheTimeOfSending { get; set; }
+    }
+}
