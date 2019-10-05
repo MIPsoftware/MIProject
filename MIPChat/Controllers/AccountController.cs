e3078de82d26043f998fd845cb9145db7de146d8
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2019-10-06 00:21:18 +0300

    Rename RegisterModel

--- MIPChat/Controllers/AccountController.cs
+++ MIPChat/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MIPChat.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,14 +12,26 @@ namespace MIPChat.Controllers
         [HttpGet]
         public ActionResult Login()
         {
-            return View();
+            return View(); 
         }
 
 
         [HttpPost]
         public ActionResult Login(LoginModel model)
         {
+            return View();
+        }
 
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterModel model)
+        {
+            return View();
         }
     }
 }
