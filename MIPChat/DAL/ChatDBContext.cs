dea590e8c86d3641fb3c8ef7f88500f906d2ed01
DaneArt <public.centaurus@gmail.com>
2019-10-05 16:03:44 +0300

    Создан базовый репозиторий и модели необходимые для регистрации

---
+++ MIPChat/DAL/ChatDBContext.cs
@@ -0,0 +1,21 @@
+﻿using MIPChat.DAL.Domain;
+using MIPChat.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MIPChat.DAL
+{
+    public class ChatDBContext: DbContext
+    {
+        public ChatDBContext(): base("ChatDBContext")
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<FileModel> Files { get; set; }
+    }
+}
---
+++ MIPChat/DAL/Domain/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MIPChat.DAL.Domain;
+
+namespace MIPChat.Models
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime LastLogIn { get; set; }
+        public DateTime LastLogOut { get; set; }
+        public byte[] Icon { get; set; }
+    }
+}
