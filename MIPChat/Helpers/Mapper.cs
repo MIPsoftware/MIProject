22b06e442ad075f68683dfc5558d37ddb9d97e7e
DaneArt <public.centaurus@gmail.com>
2019-10-05 16:03:44 +0300

    Создан базовый репозиторий и модели необходимые для регистрации

---
+++ MIPChat/Helpers/Mapper.cs
@@ -0,0 +1,39 @@
+﻿using MIPChat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIPChat.Helpers
+{
+    public static class Mapper
+    {
+        public static User Map(UserViewModel User)
+        {
+            return new User
+            {
+                UserId = User.UserId,
+                Email = User.Email,
+                Icon = User.Icon,
+                Login = User.Login,
+                Name = User.Name,
+                LastLogOut = User.LastLogOut,
+                Surname = User.Name
+            };
+        }
+
+        public static UserViewModel Map(User User)
+        {
+            return new UserViewModel
+            {
+                UserId = User.UserId,
+                Email = User.Email,
+                Icon = User.Icon,
+                Login = User.Login,
+                Name = User.Name,
+                LastLogOut = User.LastLogOut,
+                Surname = User.Name
+            };
+        }
+    }
+}
---
+++ MIPChat/Models/UserViewModel.cs
@@ -0,0 +1,19 @@
+﻿using MIPChat.DAL.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIPChat.Models
+{
+    public class UserViewModel
+    {
+        public Guid UserId { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime LastLogOut { get; set; }
+        public byte[] Icon { get; set; }
+    }
+}
