f54c3e29540d2d8289351bbce31124eae91593d1
MIPsoftware <55354070+MIPsoftware@users.noreply.github.com>
2019-10-05 21:10:13 +0300

    Update UserViewModel.cs

--- MIPChat/Models/UserViewModel.cs
+++ MIPChat/Models/UserViewModel.cs
@@ -8,9 +8,14 @@ namespace MIPChat.Models
 {
     public class UserViewModel
     {
+        [Key]
+        public Guid UserId { get; set; }
+        [EmailAddress]
+        [Required(ErrorMessage = "У пользователя должен быть Email")]
+        [Key]
+        public string Email { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
-        public DateTime LastLogOut { get; set; }
         public byte[] Icon { get; set; }
     }
 }
