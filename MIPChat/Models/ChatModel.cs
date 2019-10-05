ad3427df2cbdfeb21ea93737fd7e1c45676cf552
danil <danil@DESKTOP-SK7DR6D>
2019-10-05 20:01:44 +0300

    Добавио ChatModel и RegiterModel

---
+++ MIPChat/Models/ChatModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MIPChat.Models
+{
+    public class ChatModel
+    {
+        public int Id { get; set; }
+        public virtual ICollection<MessageModel> Messages { get; set; }
+        public virtual ICollection<UserViewModel> Users { get; set; }
+    }
+}
