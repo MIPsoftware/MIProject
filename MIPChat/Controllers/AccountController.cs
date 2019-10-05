518ae382ba7477fb2e62d3f017d7492e393f70e0
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2019-10-06 00:04:22 +0300

    Create AccountController.cs

---
+++ MIPChat/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MIPChat.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+
+        }
+    }
+}
