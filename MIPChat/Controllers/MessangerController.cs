c7584f0f100139cdd9c0a4f20274c2bc2e00ee2d
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2019-10-06 12:47:58 +0300

    Add MessangerController

--- MIPChat/Controllers/MessangerController.cs
+++ MIPChat/Controllers/MessangerController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using MIPChat.DAL;
+using MIPChat.DAL.Repository;
+using MIPChat.DAL.UnitOfWork;
+using MIPChat.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +13,42 @@ namespace MIPChat.Controllers
 {
     public class MessangerController : Controller
     {
-        
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult UserMessAndChatsAsync(User user)
+        {
+            var chatRepository = new ChatRepository(new ChatDBContext());
+            var allChats = chatRepository.FindAllChatsForUser(user).Result;
+
+            return PartialView(allChats);
+        }
+
+        [HttpPost]
+        public ActionResult AvailNewMessAndChats(Guid userID)
+        {
+            var userRepository = new UserRepository(new ChatDBContext())
+            {
+
+            }
+
+
+
+            return PartialView();
+        }
+
+
+        [HttpPost]
+        public ActionResult UserConcreteChat(int ChatID)
+        {
+
+            return PartialView();
+        }
+
+
     }
 }
