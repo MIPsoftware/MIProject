0ac43ad869507a4fc1e208c76b2239fc606dd539
martin080 <45700603+martin080@users.noreply.github.com>
2019-10-13 13:45:26 +0300

    Update IUsersRepository.cs

--- MIPChat/DAL/Repository/IUsersRepository.cs
+++ MIPChat/DAL/Repository/IUsersRepository.cs
@@ -11,5 +11,9 @@ namespace MIPChat.DAL
     public interface IUsersRepository : IRepository<User>
     {
         Task<User> FindUserByEmail(string Email);
+        Task<bool> PasswordCheck(LoginModel input);
+        Task<ICollection<User>> GetAllUsersExceptAsync(ICollection<Guid> guids);
+        Task<ICollection<Message>> GetNewMessagesAsync(Guid userId, Guid chatId);
+        Task<Dictionary<Guid, ICollection<Message>>> GetAllNewMessagesAsync(Guid userId);
     }
 }
