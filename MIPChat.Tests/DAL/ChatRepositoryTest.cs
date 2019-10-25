a115e8c2437da7e5df670192f2bf70d4f8df5463
martin080 <45700603+martin080@users.noreply.github.com>
2019-10-25 20:59:37 +0300

    little fix

---
+++ MIPChat.Tests/DAL/ChatRepositoryTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MIPChat.DAL;
+using MIPChat.DAL.UnitOfWork;
+using MIPChat.Models;
+using Moq;
+using System.Linq;
+
+namespace MIPChat.Tests.DAL
+{
+    [TestClass]
+    public class ChatRepositoryTest
+    {
+        [TestMethod]
+        public void UpdateTest() // need to test Update from BaseRepository
+        {
+            //Guid id = Guid.NewGuid();
+            //string changedName = "mip3";
+
+            //var data = new List<ChatModel> {
+            //    new ChatModel { ChatId = id, Icon = null, Name = "MIP1" },
+            //    new ChatModel { ChatId = Guid.NewGuid(), Icon = null, Name = "MIP2" }
+            //};
+
+            //var mockSet = new Mock<DbSet<ChatModel>>();
+            //var mockContext = new Mock<ChatDBContext>();
+            
+            //mockContext.Setup(a => a.Chats).Returns(mockSet.Object);
+            //mockContext.Setup(a => a.Set<ChatModel>()).Returns(mockSet.Object);
+           
+
+            //ChatRepository chatRepo = new ChatRepository(mockContext.Object);
+            //data[0].Name = changedName;
+            //chatRepo.Update(data[0]);
+            //Assert.AreEqual(data[0], chatRepo.FindById(id));
+        }
+    }
+}
---
+++ MIPChat.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MIPChat.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MIPChat.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cbaf7c51-64d9-42c1-b298-8f812bdab122")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
