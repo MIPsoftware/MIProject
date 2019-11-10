c1dfe0cee03bbd2cd0737119ffb6c130b7fe627b
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2019-11-10 19:42:59 +0300

    Bugfix

--- MIPChat/Controllers/MessangerController.cs
+++ MIPChat/Controllers/MessangerController.cs
@@ -38,7 +38,7 @@ namespace MIPChat.Controllers
         [HttpPost]
         public ActionResult FindChat(Guid ChatID)
         {
-            var chatInst = messangerData.Chats.FindById(ChatID).Messages
+            var chatInst = messangerData.Chats.FindById(ChatID).Messages;
 
             return PartialView(chatInst);
 
--- MIPChat/Hubs/OnlineHub.cs
+++ MIPChat/Hubs/OnlineHub.cs
@@ -4,14 +4,10 @@ using MIPChat.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-<<<<<<< HEAD
 using System.Web;
-using Microsoft.AspNet.SignalR;
 using MIPChat.DAL.Repository;
 using MIPChat.DAL.UnitOfWork;
-using MIPChat.Models;
-=======
->>>>>>> ee6694864790d7dfa1223a915ef6a88741873f23
+using System;
 
 namespace MIPChat.Hubs
 {
